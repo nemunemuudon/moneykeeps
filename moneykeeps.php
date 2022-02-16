62c1fa332d9fa6d56eefb3130067c0b197792f20
rina3x9 <98382554+rina3x9@users.noreply.github.com>
2022-02-16 13:50:04 +0900

    KAETA

--- moneykeeps.php
+++ moneykeeps.php
@@ -2,7 +2,7 @@
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASS","root");
-define("DB_NAME","gohan");
+define("DB_NAME","MONEEYKEEPS");
 define("DB_CHARSET","utf8mb4");
 
 //session_start();
